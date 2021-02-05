ef171ad36cc7f0db81ffe2dd036bd9169c65b334
praveen-rangasubhe <praveentrangasubhe@gmail.com>
2021-02-05 18:57:53 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello!')
+var ages = [10, 15, 18, 20];
+
+document.write(ages + "<br>");
+
+var b = ages.some(checkAdult);
+document.write(b);
+
+function checkAdult(ages) {
+  return ages >= 18;
+}
